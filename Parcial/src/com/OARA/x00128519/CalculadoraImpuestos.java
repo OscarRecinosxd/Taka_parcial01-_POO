e6f401de4f44558f7d1409a6d50b04260ca8e3d2
OscarRecinosxd <00128519@uca.edu.sv>
2020-04-17 11:37:22 -0600

    Edit CalculadoraImpuestos

--- Parcial/src/com/OARA/x00128519/CalculadoraImpuestos.java
+++ Parcial/src/com/OARA/x00128519/CalculadoraImpuestos.java
@@ -1,35 +1,48 @@
 package com.OARA.x00128519;
 
-public class CalculadoraImpuestos {
+ public class CalculadoraImpuestos {
     private static double totalRenta;
     private static double totalISSS;
     private static double totalAFP;
 
 
     private CalculadoraImpuestos() {
-        totalRenta=0;
-        totalISSS=0;
-        totalAFP=0;
     }
 
     public static double calcularPago(Empleado empleado){
+        totalRenta=0;
+        totalISSS=0;
+        totalAFP=0;
         if(empleado instanceof ServicioProfesional){
-            totalRenta= 0.1*empleado.getSalario();
+            totalRenta= 0.1*(empleado.getSalario());
             return empleado.getSalario()-totalRenta;
         }
         else{
+            double Restante;
+            totalAFP = 0.0625 * (empleado.getSalario());
+            totalISSS = 0.03 * (empleado.getSalario());
+            Restante=(empleado.getSalario())-totalAFP-totalISSS;
 
+            if(Restante >= 0.01 && Restante <= 472){
+                return Restante;
+            }
+            else if(Restante >= 472.01 && Restante <= 895.24){
+                totalRenta = 0.1*(Restante-472)+17.67;
+                return (Restante-totalRenta);
+            }
+            else if(Restante >= 985.25 && Restante <=2038.10){
+                totalRenta = 0.2*(Restante-895.24)+60;
+                return (Restante-totalRenta);
+            }
+            else if (Restante >= 2038.11 ){
+                totalRenta = 0.3*(Restante-2038.10)+288.57;
+                return (Restante-totalRenta);
+            }
             return 0;
         }
-
     }
 
-    @Override
-    public  String toString() {
-        return "CalculadoraImpuestos{" +
-                "totalRenta=" + totalRenta +
-                ", totalISSS=" + totalISSS +
-                ", totalAFP=" + totalAFP +
-                '}';
+    public static String mostrarTotales(){
+        return "Renta " + totalRenta + " ISSS " +totalISSS + " AFP " + totalAFP;
     }
 }
--- Parcial/src/com/OARA/x00128519/Main.java
+++ Parcial/src/com/OARA/x00128519/Main.java
@@ -3,6 +3,22 @@ package com.OARA.x00128519;
 public class Main {
 
     public static void main(String[] args) {
+        Empleado Persona = new ServicioProfesional("Oscar Agustin", "Gerente", 3000, 12);
+        Empleado Persona1 = new PlazaFija("Josue Arteaga", "Ingeniero informático", 2000, 2);
+        Empleado Persona2 = new PlazaFija("Oscar Agustin", "Gerente", 200, 12);
+
+        System.out.println(CalculadoraImpuestos.calcularPago(Persona));
+        System.out.println(CalculadoraImpuestos.mostrarTotales());
+
+        System.out.println(CalculadoraImpuestos.calcularPago(Persona1));
+        System.out.println(CalculadoraImpuestos.mostrarTotales());
+
+        System.out.println(CalculadoraImpuestos.calcularPago(Persona2));
+        System.out.println(CalculadoraImpuestos.mostrarTotales());
+
+
+
+
 
     }
 }
